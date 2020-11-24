f7bd31fa2791c4dd55c09a19108e0305b3276f19
Brian Liu <Bliu3847@conestogac.on.ca>
2020-11-24 15:41:11 -0500

    Added some classes (Not Finished Any of Them) -CollisionManager -UFO -PlayScene

---
+++ Prog2370-Final/CollisionManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Prog2370_Final
+{
+    public class CollisionManager : GameComponent
+    {
+        private Terrain terrain;
+        private UFO ufo;
+        private SoundEffect crash;
+        private SoundEffect land;
+        private SoundEffect gas;
+
+
+        public CollisionManager(Game game,
+            Terrain terrain,
+            UFO ufo,
+            SoundEffect crash,
+            SoundEffect land,
+            SoundEffect gas) : base(game)
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+    }
+}
---
+++ Prog2370-Final/PlayScene.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Prog2370_Final
+{
+    public class PlayScene : GameScene
+    {
+        private SpriteBatch spriteBatch;
+
+        public PlayScene(Game game) : base(game)
+        {
+        }
+    }
+}
