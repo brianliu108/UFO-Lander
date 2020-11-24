7cb73df506f1e87791a6ed661514756b9d387fff
JakeTS <timpbh@gmail.com>
2020-11-24 13:38:58 -0500

    Moved terrain to own file

--- Prog2370-Final/Game1.cs
+++ Prog2370-Final/Game1.cs
@@ -10,7 +10,6 @@ namespace Prog2370_Final {
     public class Game1 : Game {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        private VectorImage terrain;
         private VectorImage star;
 
         private readonly Color
@@ -43,28 +42,6 @@ namespace Prog2370_Final {
 
             // TODO: use this.Content to load your game content here
 
-            int samples = 80; // How many points will be used to make the curve. More points means smoother curve.
-            float range = 50; // Essentially the y scale. How far up and down will the graph go.
-            float domain = GraphicsDevice.Viewport.Bounds.Width; // The x range. Starts at 0 pixels, ends at _ pixels.
-            float period = 5; // The period of the base sine wave. Essentially how dense will the hills be.
-            float seed = 0; // The starting point of the curve. Different values will give different terrains.
-            Vector2[] vertices = new Vector2[samples];
-            for (float i = 0, x = 0; i < samples; i++, x = period * (float) PI * i / samples + seed)
-                vertices[(int) i] = new Vector2(
-                    domain * i / (samples - 1),
-                    range * -(float) (Cos(.95 * x)
-                                      - Cos(2.11 * (x - PI / 2)) / 2
-                                      + Cos(11 * x) / 7
-                                      + Cos(5.5 * x) / 4 * Cos(x - PI / 2)
-                                      - Cos(0.75 * x) / 2
-                                      + Cos(x / 3) / 2
-                                      - Cos(17 * x) / 13
-                                      + Cos(34 * x) / 13));
-
-
-            terrain = new VectorImage(this, spriteBatch, vertices, 4, new Color(130, 52, 65)) {
-                offset = new Vector2(0, GraphicsDevice.Viewport.Bounds.Height * 0.75f)
-            };
 
             star = new VectorImage(this, spriteBatch,
                 new[] {
@@ -112,12 +89,6 @@ namespace Prog2370_Final {
 
             // TODO: Add your drawing code here
 
-            terrain.Draw(gameTime);
-            star.offset = new Vector2(200, 90);
-            star.Draw(gameTime);
-            star.offset = new Vector2(260, 160);
-            star.Draw(gameTime);
-            star.offset = new Vector2(300, 120);
             star.Draw(gameTime);
             base.Draw(gameTime);
         }
---
+++ Prog2370-Final/Terrain.cs
@@ -0,0 +1,59 @@
+﻿using System.CodeDom.Compiler;
+using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using static System.Math;
+
+namespace Prog2370_Final {
+    public class Terrain : DrawableGameComponent {
+        private Game game;
+        private SpriteBatch spriteBatch;
+        private VectorImage terrain;
+
+        private int samples;
+        private float range;
+        private float domain;
+        private float period;
+        private float seed;
+
+        public Terrain(Game game, SpriteBatch spriteBatch,
+            int samples, float range, float domain, float period, float seed)
+            : base(game) {
+            this.game = game;
+            this.spriteBatch = spriteBatch;
+            this.samples = samples;
+            this.range = range;
+            this.domain = domain;
+            this.period = period;
+            this.seed = seed;
+            Generate();
+            // samples = 80; // How many points will be used to make the curve. More points means smoother curve.
+            // range = 50; // Essentially the y scale. How far up and down will the graph go.
+            // domain = GraphicsDevice.Viewport.Bounds.Width; // The x range. Starts at 0 pixels, ends at _ pixels.
+            // period = 5; // The period of the base sine wave. Essentially how dense will the hills be.
+            // seed = 0; // The starting point of the curve. Different values will give different terrains.
+        }
+
+        public void Generate() {
+            Vector2[] vertices = new Vector2[samples];
+            for (float i = 0, x = 0; i < samples; i++, x = period * (float) PI * i / samples + seed)
+                vertices[(int) i] = new Vector2(
+                    domain * i / (samples - 1),
+                    range * -(float) (Cos(.95 * x)
+                                      - Cos(2.11 * (x - PI / 2)) / 2
+                                      + Cos(11 * x) / 7
+                                      + Cos(5.5 * x) / 4 * Cos(x - PI / 2)
+                                      - Cos(0.75 * x) / 2
+                                      + Cos(x / 3) / 2
+                                      - Cos(17 * x) / 13
+                                      + Cos(34 * x) / 13));
+            terrain = new VectorImage(game, spriteBatch, vertices, 4, new Color(130, 52, 65)) {
+                offset = new Vector2(0, GraphicsDevice.Viewport.Bounds.Height * 0.75f)
+            };
+        }
+
+        public override void Draw(GameTime gameTime) {
+            terrain.Draw(gameTime);
+        }
+    }
+}
